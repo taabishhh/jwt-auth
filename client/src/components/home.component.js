4d0bbc9189cc1c27e91ac9480a325579c5ce75ac
taabishhh <taabishsutriwala@gmail.com>
2022-12-17 13:23:07 +0530

    Updates

--- client/src/components/home.component.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default function Home(props) {
-  return (
-    <div className="">
-      <h1>Home</h1>
-    </div>
-  );
-}
---
+++ precache-manifest.6cc91829bd3228e52055606819e2c153.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "222ea4ea92c5b06e78e3",
+    "url": "/static/css/main.63b795ae.chunk.css"
+  },
+  {
+    "revision": "222ea4ea92c5b06e78e3",
+    "url": "/static/js/main.222ea4ea.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "70644652f75968347995",
+    "url": "/static/css/2.beffa936.chunk.css"
+  },
+  {
+    "revision": "70644652f75968347995",
+    "url": "/static/js/2.70644652.chunk.js"
+  },
+  {
+    "revision": "b29c9e157adc6ecf5862",
+    "url": "/static/js/runtime~main.b29c9e15.js"
+  },
+  {
+    "revision": "d57ff72f4e40d09ea36931438f2b4fa0",
+    "url": "/index.html"
+  }
+];
